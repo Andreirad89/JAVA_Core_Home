38f756a58926361c5f07eef92dab449aee625a1e
Andreirad89 <80522068+Andreirad89@users.noreply.github.com>
2021-10-26 21:05:47 +0300

    Add files via upload
    HomeWork3

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
-package workhome1new;
+package workhome3;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+       Box<Apple> appleBox = new Box<>();
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        appleBox.addFruit(new Apple());
+        System.out.println("Всего в первой коробке яблок :" + appleBox.getWeight());
 
-        Course course = new Course(330,2,150);
+        Box<Orange> orangeBox = new Box<>();
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        orangeBox.addFruit(new Orange());
+        System.out.println("Всего в первой коробке апельсинов :" + orangeBox.getWeight());
 
-        Team team = new Team("Команда1",
-                new People("Андрей", 30, 300, 3, 150),
-                new People("Паша", 30, 330, 1, 100),
-                new People("Сергей", 30, 350, 3, 180),
-                new People("Иван", 30, 300, 1, 100)
-        );
 
-        course.letsGo(team);
-        team.showResults();
+        System.out.println(orangeBox.compareTo(appleBox));
+
+
+        Box<Apple> appleBox2 = new Box<>();
+
+        appleBox.pourOver(appleBox2);
+        System.out.println("В первой коробке сейчас :" + appleBox.getFruitList());
+        System.out.println("Во второй коробке сейчас :" + appleBox2.getFruitList());
+
     }
 }
+
